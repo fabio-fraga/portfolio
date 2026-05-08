c338ce0d24ca6f5ca5cc6e4d535ed609034ef09f
fabiofraga <fabio.fraga@vialoterias.com.br>
2026-05-08 12:09:24 -0300

    fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
   output: 'export',
   typescript: {
     ignoreBuildErrors: true,
